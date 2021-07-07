3ec042ddb544e3ec5197e8c849705f9993e004ad
Mouad-Gh <Mouad.ghouichat@gmail.com>
2021-07-07 23:04:03 +0100

    add models 2

--- server.js
+++ server.js
@@ -36,6 +36,9 @@ db.Produit.belongsToMany(db.Commande, { through: 'ligne_commande' });
 //chaque Acheteur peut adorer plusieurs Produits et chaque Produit peut être adorer par plusieurs Acheteurs
 db.Acheteur.belongsToMany(db.Produit, { through: 'produit_souhaite' });
 db.Produit.belongsToMany(db.Acheteur, { through: 'produit_souhaite' });
+//chaque Achteur peut effectuer plusieurs Commandes 
+db.Acheteur.hasMany(db.Commande);
+db.Commande.belongsTo(db.Acheteur);
 
 db.sequelize.sync({force: true}).then((result)=>{
     //  console.log(result);
